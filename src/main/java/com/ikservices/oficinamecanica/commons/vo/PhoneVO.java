7722b658522584c230292fd786c10bbed826ae7a
Isaias Lima <isaiaschaid@gmail.com>
2024-06-13 02:15:50 +0100

    Create new parsePhoneVO method in PhoneVO class, and Tests

--- src/main/java/com/ikservices/oficinamecanica/commons/vo/PhoneVO.java
+++ src/main/java/com/ikservices/oficinamecanica/commons/vo/PhoneVO.java
@@ -11,15 +11,15 @@ import java.util.Objects;
 @Setter
 @EqualsAndHashCode
 public class PhoneVO {
-    private final Integer coutryCode;
+    private final Integer countryCode;
     private final Integer stateCode;
     private final Integer phoneNumber;
 
-    public PhoneVO(Integer coutryCode, Integer stateCode, Integer phoneNumber) {
+    public PhoneVO(Integer countryCode, Integer stateCode, Integer phoneNumber) {
         if (Objects.isNull(stateCode) || Objects.isNull(phoneNumber)) {
             throw new IKException("Telefone inválido.");
         }
-        this.coutryCode = coutryCode;
+        this.countryCode = countryCode;
         this.stateCode = stateCode;
         this.phoneNumber = phoneNumber;
     }
@@ -27,16 +27,41 @@ public class PhoneVO {
     public String getFullPhone() {
         String phone = "";
 
-        if (Objects.nonNull(coutryCode)) {
-            phone = "+" + coutryCode + " ";
+        if (Objects.nonNull(countryCode)) {
+            phone = "+" + countryCode + " ";
         }
 
-        phone += stateCode;
+        phone += stateCode + " ";
 
         int phoneLength = String.valueOf(phoneNumber).length();
         phone += phoneLength == 9 ? String.valueOf(phoneNumber).substring(0, 5) + "-" : String.valueOf(phoneNumber).substring(0, 4) + "-";
         phone += phoneLength == 9 ? String.valueOf(phoneNumber).substring(5, phoneLength) : String.valueOf(phoneNumber).substring(4, phoneLength);
 
         return phone;
+    }
+
+    /**
+     * <p>Parse String to PhoneVO</p>
+     * The parameter fullPhone must be Formatted like it: <br>
+     * <strong style='color:yellow'>+00 11 92222-3333</strong> for Mobile phone, and, <br>
+     * <strong style='color:yellow'>+00 11 2222-3333</strong> for landline.
+     * @param fullPhone String
+     * @return PhoneVO
+     */
+    public static PhoneVO parsePhoneVO(String fullPhone) {
+
+        if (Objects.nonNull(fullPhone) && fullPhone.length() >= 16) {
+
+            boolean valid = fullPhone.contains("+") && fullPhone.contains(" ") && fullPhone.contains("-");
+
+            if (valid) {
+                String[] phoneArray = fullPhone.split(" ");
+                Integer countryCode = Integer.parseInt(phoneArray[0].replace("+", ""));
+                Integer stateCode = Integer.parseInt(phoneArray[1]);
+                Integer phoneNumber = Integer.parseInt(phoneArray[2].replace("-", ""));
+                return new PhoneVO(countryCode, stateCode, phoneNumber);
+            }
+        }
+        return null;
     }
 }
