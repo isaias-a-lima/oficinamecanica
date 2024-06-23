43077e53c4d0145a3e32f5b37e0ed83082490df5
mateuscamargodev <mateuscamargo929@gmail.com>
2024-06-23 11:22:50 -0300

    Create ServiceRepository interface

---
+++ src/main/java/com/ikservices/oficinamecanica/services/application/gateways/ServiceRepository.java
@@ -0,0 +1,13 @@
+package com.ikservices.oficinamecanica.services.application.gateways;
+
+import java.util.List;
+
+import com.ikservices.oficinamecanica.services.domain.Service;
+
+public interface ServiceRepository {
+	Service saveService(Service service);
+	Service updateService(Service service);
+	Service getService(Long id);
+	List<Service> getServiceList(Long workshopId);
+	void deleteService(Long id);
+}
