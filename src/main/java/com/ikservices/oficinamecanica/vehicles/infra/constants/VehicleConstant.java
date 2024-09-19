70e67aefdb60d293a24909e95abcf6b53c912193
mateuscamargodev <mateuscamargo929@gmail.com>
2024-09-19 17:17:32 -0300

    Vehicles - Create constant and properties

---
+++ src/main/java/com/ikservices/oficinamecanica/vehicles/infra/constants/VehicleConstant.java
@@ -0,0 +1,9 @@
+package com.ikservices.oficinamecanica.vehicles.infra.constants;
+
+public class VehicleConstant {
+	public static final String SAVE_SUCCESS_MESSAGE = "vehicle.save.success.message";
+	public static final String UPDATE_SUCCESS_MESSAGE  = "vehicle.update.success.message";
+	public static final String DELETE_SUCCESS_MESSAGE = "vehicle.delete.success.message";
+	public static final String VEHICLE_ALREADY_RECORDED_TO_THIS_CUSTOMER = 
+			"vehicle.ja.se.encontra.registrado.para.esse.cliente";
+}
