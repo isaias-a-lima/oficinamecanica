5cfccd4a7023ae2d9093a6ddc9018061db711f91
mateuscamargodev <mateuscamargo929@gmail.com>
2024-10-16 20:09:10 -0300

    messageRefactoring -  refactor services and parts messages

--- src/main/java/com/ikservices/oficinamecanica/parts/infra/controller/PartController.java
+++ src/main/java/com/ikservices/oficinamecanica/parts/infra/controller/PartController.java
@@ -6,6 +6,10 @@ import java.util.Objects;
 
 import javax.transaction.Transactional;
 
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +22,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import com.ikservices.oficinamecanica.commons.exception.IKException;
 import com.ikservices.oficinamecanica.commons.response.IKResponse;
+import com.ikservices.oficinamecanica.commons.utils.IKLoggerUtil;
 import com.ikservices.oficinamecanica.parts.application.usecases.GetNextPartId;
 import com.ikservices.oficinamecanica.parts.application.usecases.GetPart;
 import com.ikservices.oficinamecanica.parts.application.usecases.GetPartsList;
@@ -26,11 +31,18 @@ import com.ikservices.oficinamecanica.parts.application.usecases.UpdatePart;
 import com.ikservices.oficinamecanica.parts.domain.Part;
 import com.ikservices.oficinamecanica.parts.domain.PartId;
 import com.ikservices.oficinamecanica.parts.infra.PartConverter;
+import com.ikservices.oficinamecanica.parts.infra.constants.PartConstant;
+import com.ikservices.oficinamecanica.services.infra.constants.ServiceConstant;
 import com.ikservices.oficinamecanica.services.infra.controller.ServiceDTO;
 
 @RestController
 @RequestMapping("/parts")
 public class PartController {
+	
+	private static final Logger LOGGER = IKLoggerUtil.getLogger(PartController.class);
+	@Autowired
+	private Environment environment;
+	
 	private final PartConverter converter;
 	private final GetPart getPart;
 	private final GetPartsList getPartsList;
@@ -52,17 +64,33 @@ public class PartController {
 	@GetMapping("/{workshopId}/{id}")
 	public ResponseEntity<IKResponse<PartDTO>> getPart(@PathVariable("workshopId") 
 	Long workshopId, @PathVariable("id") Long id) {
-		Part part = getPart.execute(new PartId(id, workshopId));
-		return ResponseEntity.ok(IKResponse.<PartDTO>build().body(new PartDTO(part)));
+		try {
+			Part part = getPart.execute(new PartId(id, workshopId));
+			return ResponseEntity.ok(IKResponse.<PartDTO>build().body(new PartDTO(part)));			
+		}catch(IKException ike) {
+			LOGGER.error(ike.getMessage(), ike);
+			return ResponseEntity.status(ike.getCode()).body(IKResponse.<PartDTO>build().addMessage(ike.getIKMessageType(), ike.getMessage()));
+		}catch(Exception e) {
+			LOGGER.error(environment.getProperty(PartConstant.OPERATION_ERROR_MESSAGE));
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(IKResponse.<PartDTO>build().addMessage(environment.getProperty(PartConstant.DEFAULT_SERVER_ERROR_MESSAGE)));
+		}
 	}
 	
 	@GetMapping("/workshop/{id}")
 	public ResponseEntity<IKResponse<List<PartDTO>>> getPartList(@PathVariable("id") Long workshopId) {
-		 List<PartDTO> partsList = null;
-		 
-		 partsList = converter.parseDTOList(getPartsList.execute(workshopId));
-		 
-		 return ResponseEntity.ok(IKResponse.<List<PartDTO>>build().body(partsList));
+		try {
+			List<PartDTO> partsList = null;
+			
+			partsList = converter.parseDTOList(getPartsList.execute(workshopId));
+			
+			return ResponseEntity.ok(IKResponse.<List<PartDTO>>build().body(partsList));			
+		}catch(IKException ike) {
+			LOGGER.error(ike.getMessage(), ike);
+			return ResponseEntity.status(ike.getCode()).body(IKResponse.<List<PartDTO>>build().addMessage(ike.getIKMessageType(), ike.getMessage()));
+		}catch(Exception e) {
+			LOGGER.error(environment.getProperty(PartConstant.OPERATION_ERROR_MESSAGE));
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(IKResponse.<List<PartDTO>>build().addMessage(environment.getProperty(PartConstant.DEFAULT_SERVER_ERROR_MESSAGE)));
+		}
 	}
 	
 	@PostMapping()
@@ -79,13 +107,24 @@ public class PartController {
 		}catch(IKException ike) {
 			int code = Objects.nonNull(ike.getCode()) ? ike.getCode() : 500;
 			return ResponseEntity.status(code).body(IKResponse.<PartDTO>build().addMessage(ike.getIKMessageType(), ike.getMessage()));
-		}		
+		}catch(Exception e) {
+			LOGGER.error(environment.getProperty(PartConstant.OPERATION_ERROR_MESSAGE));
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(IKResponse.<PartDTO>build().addMessage(environment.getProperty(PartConstant.DEFAULT_SERVER_ERROR_MESSAGE)));
+		}
 	}
 	
 	@PutMapping
 	@Transactional
 	public ResponseEntity<IKResponse<PartDTO>> updatePart(@RequestBody PartDTO dto) {
-		Part part = updatePart.execute(converter.parsePart(dto));
-		return ResponseEntity.ok(IKResponse.<PartDTO>build().body(new PartDTO(part)));
+		try {
+			Part part = updatePart.execute(converter.parsePart(dto));
+			return ResponseEntity.ok(IKResponse.<PartDTO>build().body(new PartDTO(part)));			
+		}catch(IKException ike) {
+			int code = Objects.nonNull(ike.getCode()) ? ike.getCode() : 500;
+			return ResponseEntity.status(code).body(IKResponse.<PartDTO>build().addMessage(ike.getIKMessageType(), ike.getMessage()));
+		}catch(Exception e) {
+			LOGGER.error(environment.getProperty(PartConstant.OPERATION_ERROR_MESSAGE));
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(IKResponse.<PartDTO>build().addMessage(environment.getProperty(PartConstant.DEFAULT_SERVER_ERROR_MESSAGE)));
+		}
 	}
 }
--- src/main/java/com/ikservices/oficinamecanica/services/infra/controller/ServiceController.java
+++ src/main/java/com/ikservices/oficinamecanica/services/infra/controller/ServiceController.java
@@ -8,12 +8,18 @@ import javax.persistence.Convert;
 import javax.persistence.Converter;
 import javax.transaction.Transactional;
 
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.ikservices.oficinamecanica.commons.exception.IKException;
+import com.ikservices.oficinamecanica.commons.response.IKRes;
 import com.ikservices.oficinamecanica.commons.response.IKResponse;
+import com.ikservices.oficinamecanica.commons.utils.IKLoggerUtil;
 import com.ikservices.oficinamecanica.services.application.usecases.GetNextServiceId;
 import com.ikservices.oficinamecanica.services.application.usecases.GetService;
 import com.ikservices.oficinamecanica.services.application.usecases.ListServices;
@@ -22,10 +28,18 @@ import com.ikservices.oficinamecanica.services.application.usecases.UpdateServic
 import com.ikservices.oficinamecanica.services.domain.Service;
 import com.ikservices.oficinamecanica.services.domain.ServiceId;
 import com.ikservices.oficinamecanica.services.infra.ServiceConverter;
+import com.ikservices.oficinamecanica.services.infra.constants.ServiceConstant;
+import com.ikservices.oficinamecanica.vehicles.infra.constants.VehicleConstant;
+import com.ikservices.oficinamecanica.vehicles.infra.controller.VehicleDTO;
+import com.ikservices.oficinamecanica.vehicles.infra.controller.VehicleResponse;
 
 @RestController
 @RequestMapping("/services")
 public class ServiceController {
+	
+	private static final Logger LOGGER = IKLoggerUtil.getLogger(ServiceController.class);
+	@Autowired
+	private Environment environment;
 	
 	private final ServiceConverter converter;
 	private final GetService getService;
@@ -47,8 +61,16 @@ public class ServiceController {
 	@GetMapping("/{workshopId}/{id}")
 	public ResponseEntity<IKResponse<ServiceDTO>> getService(@PathVariable("workshopId") Long workshopId,
 			@PathVariable("id") Long id){
-		Service service = getService.execute(new ServiceId(id, workshopId));
-		return ResponseEntity.ok(IKResponse.<ServiceDTO>build().body(new ServiceDTO(service)));
+		try {
+			Service service = getService.execute(new ServiceId(id, workshopId));
+			return ResponseEntity.ok(IKResponse.<ServiceDTO>build().body(new ServiceDTO(service)));	
+		}catch(IKException ike) {
+			LOGGER.error(ike.getMessage(), ike);
+			return ResponseEntity.status(ike.getCode()).body(IKResponse.<ServiceDTO>build().addMessage(ike.getIKMessageType(), ike.getMessage()));
+		}catch(Exception e) {
+			LOGGER.error(environment.getProperty(ServiceConstant.OPERATION_ERROR_MESSAGE));
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(IKResponse.<ServiceDTO>build().addMessage(environment.getProperty(ServiceConstant.DEFAULT_SERVER_ERROR_MESSAGE)));
+		}
 	}
 	
 	@GetMapping("/workshop/{id}")
@@ -57,7 +79,11 @@ public class ServiceController {
 			List<ServiceDTO> serviceList = converter.parseServiceDTOList(listServices.execute(workshopId, search));
 			return ResponseEntity.ok(IKResponse.<ServiceDTO>build().body(serviceList));
 		} catch (IKException ike) {
+			LOGGER.error(environment.getProperty(ServiceConstant.OPERATION_ERROR_MESSAGE));
 			return ResponseEntity.status(ike.getCode()).body(IKResponse.<ServiceDTO>build().addMessage(ike.getIKMessageType(), ike.getMessage()));
+		} catch (Exception e) {
+			LOGGER.error(environment.getProperty(ServiceConstant.OPERATION_ERROR_MESSAGE));
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(IKResponse.<ServiceDTO>build().addMessage(environment.getProperty(VehicleConstant.DEFAULT_SERVER_ERROR_MESSAGE)));
 		}
 	}
 	
@@ -68,19 +94,33 @@ public class ServiceController {
 			Long nextServiceId = this.getNextServiceId.execute(serviceDTO.getWorkshopId());
 			serviceDTO.setServiceId(nextServiceId);
 			savedService = saveService.execute(converter.parseService(serviceDTO));
+			
+			URI uri = uriBuilder.path("/services/{workshopId}/{id}").buildAndExpand(savedService.getId().getWorkshopId(),
+					savedService.getId().getId()).toUri();
+			return ResponseEntity.created(uri).body(IKResponse.<ServiceDTO>build().body(new ServiceDTO(savedService)));
 		} catch(IKException ike) {
 			int code = Objects.nonNull(ike.getCode()) ? ike.getCode() : 500;
+			LOGGER.error(environment.getProperty(ServiceConstant.OPERATION_ERROR_MESSAGE));
 			return ResponseEntity.status(code).body(IKResponse.<ServiceDTO>build().addMessage(ike.getIKMessageType(), ike.getMessage()));
+		}catch(Exception e) {
+			LOGGER.error(environment.getProperty(ServiceConstant.OPERATION_ERROR_MESSAGE));
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(IKResponse.<ServiceDTO>build().addMessage(environment.getProperty(VehicleConstant.DEFAULT_SERVER_ERROR_MESSAGE)));
 		}
-		URI uri = uriBuilder.path("/services/{workshopId}/{id}").buildAndExpand(savedService.getId().getWorkshopId(),
-				savedService.getId().getId()).toUri();
-		return ResponseEntity.created(uri).body(IKResponse.<ServiceDTO>build().body(new ServiceDTO(savedService)));		
 	}
 	
 	@PutMapping
 	@Transactional
-	public ResponseEntity<IKResponse<ServiceDTO>> updateService(@RequestBody	 ServiceDTO serviceDTO){
-		Service service = updateService.execute(converter.parseService(serviceDTO));
-		return ResponseEntity.ok(IKResponse.<ServiceDTO>build().body(new ServiceDTO(service)));
+	public ResponseEntity<IKResponse<ServiceDTO>> updateService(@RequestBody ServiceDTO serviceDTO){
+		try {
+			Service service = updateService.execute(converter.parseService(serviceDTO));
+			return ResponseEntity.ok(IKResponse.<ServiceDTO>build().body(new ServiceDTO(service)));			
+		}catch(IKException ike) {
+			int code = Objects.nonNull(ike.getCode()) ? ike.getCode() : 500;
+			LOGGER.error(environment.getProperty(ServiceConstant.OPERATION_ERROR_MESSAGE));
+			return ResponseEntity.status(code).body(IKResponse.<ServiceDTO>build().addMessage(ike.getIKMessageType(), ike.getMessage()));
+		}catch(Exception e) {
+			LOGGER.error(environment.getProperty(ServiceConstant.OPERATION_ERROR_MESSAGE));
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(IKResponse.<ServiceDTO>build().addMessage(environment.getProperty(VehicleConstant.DEFAULT_SERVER_ERROR_MESSAGE)));
+		}
 	}
 }
