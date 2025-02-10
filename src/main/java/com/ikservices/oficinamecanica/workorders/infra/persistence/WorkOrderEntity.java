d4912a49843730b0180007acbfc287758cf54936
Isaias Lima <isaiaschaid@gmail.com>
2025-02-10 00:50:30 +0000

    Work Order Part Items - Create update business rules

--- src/main/java/com/ikservices/oficinamecanica/workorders/infra/persistence/WorkOrderEntity.java
+++ src/main/java/com/ikservices/oficinamecanica/workorders/infra/persistence/WorkOrderEntity.java
@@ -1,6 +1,8 @@
 package com.ikservices.oficinamecanica.workorders.infra.persistence;
 
 import com.ikservices.oficinamecanica.budgets.infra.persistence.BudgetEntity;
+import com.ikservices.oficinamecanica.budgets.items.parts.infra.persistence.BudgetItemPartEntity;
+import com.ikservices.oficinamecanica.budgets.items.services.infra.persistence.BudgetItemServiceEntity;
 import com.ikservices.oficinamecanica.workorders.domain.enumarates.PayFormEnum;
 import com.ikservices.oficinamecanica.workorders.domain.enumarates.WorkOrderStatusEnum;
 import com.ikservices.oficinamecanica.workorders.installments.infra.persistence.WorkOrderInstallmentEntity;
@@ -15,6 +17,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Getter
 @Setter
@@ -56,6 +59,8 @@ public class WorkOrderEntity {
 	@OneToMany(mappedBy = "workOrder", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<WorkOrderInstallmentEntity> installments;
 
+	//TODO Here will be the service items list
+
 	@OneToMany(mappedBy = "workOrder", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<WorkOrderPartItemEntity> partItems;
 	
@@ -66,14 +71,69 @@ public class WorkOrderEntity {
 		if(Objects.nonNull(entity.getWoStatus())) {
 			this.woStatus = entity.getWoStatus();
 		}
-		if(Objects.nonNull(entity.getPaid())) {
-			this.paid = entity.getPaid();
-		}
 		if(Objects.nonNull(entity.getPayForm())) {
 			this.payForm = entity.getPayForm();
 		}
 		if(Objects.nonNull(entity.getPayQty())) {
 			this.payQty = entity.getPayQty();
 		}
+
+		//TODO There will be the invocation of service items update here.
+
+		this.updatePartItems(entity.getPartItems());
 	}
+
+	private void updateAmount() {
+		BigDecimal sum = BigDecimal.ZERO;
+
+		//TODO There will be a FOR of service items here.
+
+		for (WorkOrderPartItemEntity partItem : partItems) {
+			sum = sum.add(partItem.getTotal());
+		}
+		this.amount = sum;
+	}
+
+	//Service items begin
+	//TODO Here will be the service items update business rules.
+	//Service items end
+
+	//Part items begin
+	private void updatePartItems(List<WorkOrderPartItemEntity> newItems) {
+
+		this.partItems.removeIf(existingItem ->
+				newItems.stream().noneMatch(newItem -> newItem.getId().equals(existingItem.getId()))
+		);
+
+		for (WorkOrderPartItemEntity newItem : newItems) {
+
+			Optional<WorkOrderPartItemEntity> existingItemOptional = this.partItems.stream()
+					.filter(existingItem -> existingItem.getId().equals(newItem.getId()))
+					.findFirst();
+
+			if (existingItemOptional.isPresent()) {
+				WorkOrderPartItemEntity existingItem = existingItemOptional.get();
+				existingItem.update(newItem);
+			} else {
+				this.partItemIncluding(newItem);
+			}
+		}
+
+		this.updateAmount();
+	}
+
+	public void addPartItem(WorkOrderPartItemEntity partItem) {
+		this.partItemIncluding(partItem);
+		this.updateAmount();
+	}
+
+	private void partItemIncluding(WorkOrderPartItemEntity partItem) {
+		partItem.getId().setItemId(getNextPartId());
+		this.partItems.add(partItem);
+	}
+
+	private Long getNextPartId() {
+		return this.partItems.isEmpty() ? 1 : this.partItems.get(this.partItems.size() - 1).getId().getItemId() + 1;
+	}
+	//Part items end
 }
