ac9ebaa2a123f1bba5606a79ecd4937fd60ab390
mateuscamargodev <mateuscamargo929@gmail.com>
2025-03-10 17:33:10 -0300

    Categories - create save functionality

--- src/main/java/com/ikservices/oficinamecanica/categories/infra/controller/CategoryController.java
+++ src/main/java/com/ikservices/oficinamecanica/categories/infra/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.ikservices.oficinamecanica.categories.infra.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -10,9 +11,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.ikservices.oficinamecanica.categories.application.usecases.GetCategory;
 import com.ikservices.oficinamecanica.categories.application.usecases.GetNextCategoryId;
@@ -28,6 +33,8 @@ import com.ikservices.oficinamecanica.commons.exception.IKException;
 import com.ikservices.oficinamecanica.commons.response.IKMessageType;
 import com.ikservices.oficinamecanica.commons.response.IKResponse;
 import com.ikservices.oficinamecanica.commons.utils.IKLoggerUtil;
+import com.ikservices.oficinamecanica.suppliers.infra.constants.SupplierConstants;
+import com.ikservices.oficinamecanica.suppliers.infra.controller.SupplierDTO;
 
 import lombok.Getter;
 
@@ -97,5 +104,34 @@ public class CategoryController {
 					body(IKResponse.<CategoryDTO>build().addMessage(Constants.DEFAULT_ERROR_CODE, 
 							IKMessageType.ERROR, environment.getProperty(CategoryConstant.GET_ERROR_MESSAGE)));
 		}
+	}
+
+	@PostMapping()
+	public ResponseEntity<IKResponse<CategoryDTO>> saveCategory(@RequestBody CategoryDTO categoryDTO, 
+			UriComponentsBuilder uriBuilder) {
+		try {			
+			Category category = null;
+			
+			Integer nextId = getNextCategoryId.execute(categoryDTO.getWorkshopId());
+			categoryDTO.setCategoryId(nextId);
+			
+			category = saveCategory.execute(converter.parseRequestToDomain(categoryDTO));
+			URI uri = uriBuilder.path("categories/{categoryId}/{workshopId}").
+					buildAndExpand(category.getId().getCategoryId(), category.getId().getWorkshopId()).
+					toUri();
+			
+			return ResponseEntity.created(uri).body(IKResponse.<CategoryDTO>build().
+					body(converter.parseDomainToResponse(category)).addMessage(Constants.DEFAULT_SUCCESS_CODE,
+							IKMessageType.SUCCESS, environment.getProperty(CategoryConstant.SAVE_SUCCESS_MESSAGE)));
+		}catch(IKException ike) {
+            LOGGER.error(ike.getMessage(), ike);
+			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(IKResponse.<CategoryDTO>build()
+					.addMessage(ike.getIkMessage().getCode(), IKMessageType.getByCode(ike.getIkMessage().getType()), ike.getIkMessage().getMessage()));
+		} catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+					IKResponse.<CategoryDTO>build().addMessage(Constants.DEFAULT_ERROR_CODE, IKMessageType.ERROR, 
+							environment.getProperty(CategoryConstant.SAVE_SUCCESS_MESSAGE)));
+        }
 	}
 }
