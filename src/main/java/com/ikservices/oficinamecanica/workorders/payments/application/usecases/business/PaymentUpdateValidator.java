ebde2e288ffd847a3bd332d3ee2450303a5d716a
Isaias Lima <isaiaschaid@gmail.com>
2026-07-07 18:18:33 +0100

    [Payment] Create a validation to check whether there is any remaining payables

--- src/main/java/com/ikservices/oficinamecanica/workorders/payments/application/usecases/business/PaymentUpdateValidator.java
+++ src/main/java/com/ikservices/oficinamecanica/workorders/payments/application/usecases/business/PaymentUpdateValidator.java
@@ -9,9 +9,13 @@ import com.ikservices.oficinamecanica.workorders.domain.WorkOrder;
 import com.ikservices.oficinamecanica.workorders.domain.enumarates.WorkOrderStatusEnum;
 import com.ikservices.oficinamecanica.workorders.payments.application.gateways.IPaymentBusiness;
 import com.ikservices.oficinamecanica.workorders.payments.domain.Payment;
+import com.ikservices.oficinamecanica.workorders.payments.domain.PaymentId;
+import com.ikservices.oficinamecanica.workorders.payments.domain.PaymentTypeEnum;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public class PaymentUpdateValidator implements IPaymentBusiness {
@@ -42,6 +46,9 @@ public class PaymentUpdateValidator implements IPaymentBusiness {
             BigDecimal aux = BigDecimal.ZERO;
             BigDecimal amountPaid = BigDecimal.ZERO;
 
+            int lastId = 0;
+            boolean lastPaid = false;
+
             for (Payment payment : paymentList) {
                 aux = aux.add(payment.getPaymentValue());
 
@@ -55,6 +62,30 @@ public class PaymentUpdateValidator implements IPaymentBusiness {
 
                 if (!WorkOrderStatusEnum.DONE.equals(workOrder.getWorkOrderStatus()) && amountPaid.compareTo(workOrder.getFinalValue()) == 0) {
                     throw new IKException(new IKMessage(Constants.DEFAULT_ERROR_CODE, IKMessageType.WARNING.getCode(), CANNOT_BE_PAID));
+                }
+
+                lastId = payment.getId().getNumber();
+                lastPaid = null != payment.getPayDate();
+            }
+
+            if (aux.compareTo(workOrder.getFinalValue()) < 0) {
+
+                final int finalLastId = lastId;
+
+                if (!lastPaid) {
+                    Payment payment = paymentList.stream().filter(p -> p.getId().getNumber() == finalLastId).findFirst().orElseThrow(NoSuchElementException::new);
+                    payment.setPaymentValue(workOrder.getFinalValue().subtract(aux));
+                    int index = paymentList.indexOf(payment);
+
+                    paymentList.set(index, payment);
+                } else {
+                    Payment newPayment = new Payment();
+                    newPayment.setId(new PaymentId(finalLastId + 1, workOrder.getId().getWorkOrderId(), workOrder.getId().getBudgetId()));
+                    newPayment.setDueDate(LocalDate.now());
+                    newPayment.setPaymentValue(workOrder.getFinalValue().subtract(aux));
+                    newPayment.setPaymentType(PaymentTypeEnum.NONE);
+
+                    paymentList.add(newPayment);
                 }
             }
         }
